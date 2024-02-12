7980f8d3aa9bf840995c6dcf64a4ee7662852161
Elowan Audouin-le Guyader <elowan.aud@proton.me>
2024-02-12 18:01:17 +0100

    fix: change the stylesheet zip url

--- template.rb
+++ template.rb
@@ -3,5 +3,5 @@
 run 'rm -rf app/assets/stylesheets'
 run "curl -L https://github.com/EloItsMe/rails-stylesheet-structure/archive/refs/tags/release.zip > stylesheets.zip"
 run "unzip stylesheets.zip -d app/assets && rm -f stylesheets.zip"
-run "mv app/assets/rails-stylesheet-structure app/assets/stylesheets"
+run "mv app/assets/rails-stylesheet-structure-release app/assets/stylesheets"
 ########################################
