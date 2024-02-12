1e6b58bdb8b7bf9595c55864b944c2a8304f229c
Elowan Audouin-le Guyader <elowan.aud@proton.me>
2024-02-12 21:19:41 +0100

    feat: add test framework Gems

--- template.rb
+++ template.rb
@@ -5,3 +5,23 @@ run "curl -L https://github.com/EloItsMe/rails-stylesheet-structure/archive/refs
 run "unzip stylesheets.zip -d app/assets && rm -f stylesheets.zip"
 run "mv app/assets/rails-stylesheet-structure-release app/assets/stylesheets"
 ########################################
+
+# Test Framework
+########################################
+gem_group :development, :test do
+  gem "dotenv-rails"
+  gem 'rspec-rails'
+  gem 'factory_bot_rails'
+  gem 'faker'
+end
+
+gem_group :test do
+  gem 'shoulda-matchers'
+  gem 'database_cleaner-active_record'
+  gem 'simplecov', require: false
+end
+
+after_bundle do
+  generate "rspec:install"
+end
+########################################
