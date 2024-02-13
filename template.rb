3377d137e6427bdcb51d0978630baf3d16cb0835
Elowan Audouin-le Guyader <elowan.aud@proton.me>
2024-02-13 20:28:19 +0100

    fix: synthax error

--- template.rb
+++ template.rb
@@ -61,14 +61,12 @@ after_bundle do
     YAML
   end
 
-  insert_into_file 'config/environments/development.rb' do
+  insert_into_file 'config/environments/development.rb', after: "Rails.application.configure do\n" do
     <<~RUBY
-      Rails.application.configure do
-        config.generators.after_generate do |files|
-          parsable_files = files.filter { |file| file.end_with?('.rb') }
-          unless parsable_files.empty?
-            system("bundle exec rubocop -A --fail-level=E #{parsable_files.shelljoin}", exception: true)
-          end
+      config.generators.after_generate do |files|
+        parsable_files = files.filter { |file| file.end_with?('.rb') }
+        unless parsable_files.empty?
+          system("bundle exec rubocop -A --fail-level=E #{parsable_files.shelljoin}", exception: true)
         end
       end
     RUBY
